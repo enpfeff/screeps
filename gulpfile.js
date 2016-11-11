b124b72042329d9b89dd66623118d899fdec6b03
Ian Pfeffer <ian.pfeffer@bbiengineering.com>
2016-11-10 17:41:32 -0700

    git rib of idea

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,16 @@
 /**
  * Created by enpfeff on 11/10/16.
  */
+const screeps = require('gulp-screeps');
+const gulp = require('gulp');
+const credentials = require('./credential.json');
+
+const config = {
+    src: ['main.js']
+};
+
+function deploy() {
+    return gulp.src(config.src).pipe(screeps(credentials));
+}
+
+gulp.task('deploy', deploy);
