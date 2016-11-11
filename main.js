57e1c65ddc234ef741106b63c6e60b56f55de16e
Ian Pfeffer <ian.pfeffer@bbiengineering.com>
2016-11-10 23:01:18 -0700

    seperate our logic a bit more

--- main.js
+++ main.js
@@ -5,7 +5,7 @@
 const CreepFactory = require('creep.factory');
 
 function loop() {
-    // go get all the creeps and execute there functions
+    // for all the rooms let the creep factory manage
     const roomControllers = _.groupBy(_.map(Game.rooms, CreepFactory), 'id');
 }
 
